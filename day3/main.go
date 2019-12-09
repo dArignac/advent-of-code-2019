d1d587053b828c064f58420efa931e4d93423198
Alexander Herrmann <darignac@gmail.com>
2019-12-09 20:43:24 +0100

    started with day 3

---
+++ day3/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	"github.com/dArignac/advent-of-code-2019/day3/part1"
+)
+
+func loadInput() ([]string, error) {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var content []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		content = append(content, scanner.Text())
+	}
+
+	return content, nil
+}
+
+func main() {
+	fmt.Println("Advent of Code - Day 3 - Part 1")
+	fmt.Println("===============================")
+
+	input, err := loadInput()
+	if err != nil {
+		fmt.Println("Error occurred", err)
+		return
+	}
+
+	part1.CalculateNearestCrossingDistance(input)
+}
---
+++ day3/part1/part1.go
@@ -0,0 +1,15 @@
+package part1
+
+// find crossings
+// calculate path to crossings
+// choose the one with the shortest path
+
+// CalculateNearestCrossingDistance calculates the distance to the shortest crossing
+func CalculateNearestCrossingDistance(lines []string) int {
+	return 0
+}
+
+// TODO split string to elements (use a struct?)
+// TODO transfer to a matrix (use array or struct)
+// TODO on insertion check for crossings and remember
+// TODO calculate distances for crossings and return the shortest
