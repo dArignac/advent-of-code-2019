574624d2caff354d6de73ba2d93b28c42e0242b4
Alexander Herrmann <darignac@gmail.com>
2019-12-16 10:37:42 +0100

    added function to create coordinates for a path

--- day3/part1/part1.go
+++ day3/part1/part1.go
@@ -5,29 +5,59 @@ import (
 	"strings"
 )
 
-// find crossings
-// calculate path to crossings
-// choose the one with the shortest path
-
 type path struct {
 	direction string
 	length    int
 }
 
+type point struct {
+	x int16
+	y int16
+}
+
 // CalculateNearestCrossingDistance calculates the distance to the shortest crossing
-func CalculateNearestCrossingDistance(lines []string) int {
-	for _, line := range lines {
-		_, err := splitInputToPath(line)
+func CalculateNearestCrossingDistance(wires []string) int {
+	coordinates := []point{{x: 0, y: 0}}
+	var crossings []point
+
+	for _, wire := range wires {
+		paths, err := splitInputToPath(wire)
 		if err != nil {
 			return -1
 		}
+
+		for _, path := range paths {
+			insertPathCoordinates(path, &coordinates, &crossings)
+		}
 	}
+
+	// FIXME calculate taxi distance for existing crossing, return the shortest
+
 	return 0
 }
 
-// TODO transfer to a matrix (use array or struct)
-// TODO on insertion check for crossings and remember
-// TODO calculate distances for crossings and return the shortest
+func insertPathCoordinates(path path, coordinates *[]point, crossings *[]point) {
+	// create points slice from path
+	// check if any of the points exists, that then marks a crossing, add this to crossing
+	// add the points to the coordinates
+}
+
+func pathToPoints(path path, start point) []point {
+	result := make([]point, 0)
+	for i := 1; i <= path.length; i++ {
+		switch path.direction {
+		case "R":
+			result = append(result, point{x: start.x + int16(i), y: start.y})
+		case "D":
+			result = append(result, point{x: start.x, y: start.y - int16(i)})
+		case "L":
+			result = append(result, point{x: start.x - int16(i), y: start.y})
+		case "U":
+			result = append(result, point{x: start.x, y: start.y + int16(i)})
+		}
+	}
+	return result
+}
 
 // splitInputToPath splits the given string into "path" instances
 func splitInputToPath(input string) ([]path, error) {
--- day3/part1/part1_test.go
+++ day3/part1/part1_test.go
@@ -18,3 +18,53 @@ func TestSplitInputToPath(t *testing.T) {
 	assert.Nil(t, e1)
 	assert.Equal(t, r1, []path{path{"R", 1}, path{"L", 2}, path{"X", 99}})
 }
+
+func TestPathToPoint(t *testing.T) {
+	s1 := point{x: 0, y: 0}
+	p1 := path{direction: "R", length: 3}
+	e1 := []point{{x: 1, y: 0}, {x: 2, y: 0}, {x: 3, y: 0}}
+	r1 := pathToPoints(p1, s1)
+	assert.Equal(t, e1, r1)
+
+	s2 := point{x: -66, y: 128}
+	p2 := path{direction: "R", length: 3}
+	e2 := []point{{x: -65, y: 128}, {x: -64, y: 128}, {x: -63, y: 128}}
+	r2 := pathToPoints(p2, s2)
+	assert.Equal(t, e2, r2)
+
+	s3 := point{x: 0, y: 0}
+	p3 := path{direction: "D", length: 2}
+	e3 := []point{{x: 0, y: -1}, {x: 0, y: -2}}
+	r3 := pathToPoints(p3, s3)
+	assert.Equal(t, e3, r3)
+
+	s4 := point{x: 7, y: 1}
+	p4 := path{direction: "D", length: 3}
+	e4 := []point{{x: 7, y: 0}, {x: 7, y: -1}, {x: 7, y: -2}}
+	r4 := pathToPoints(p4, s4)
+	assert.Equal(t, e4, r4)
+
+	s5 := point{x: 0, y: 0}
+	p5 := path{direction: "L", length: 2}
+	e5 := []point{{x: -1, y: 0}, {x: -2, y: 0}}
+	r5 := pathToPoints(p5, s5)
+	assert.Equal(t, e5, r5)
+
+	s6 := point{x: -7, y: 6}
+	p6 := path{direction: "L", length: 3}
+	e6 := []point{{x: -8, y: 6}, {x: -9, y: 6}, {x: -10, y: 6}}
+	r6 := pathToPoints(p6, s6)
+	assert.Equal(t, e6, r6)
+
+	s7 := point{x: 0, y: 0}
+	p7 := path{direction: "U", length: 2}
+	e7 := []point{{x: 0, y: 1}, {x: 0, y: 2}}
+	r7 := pathToPoints(p7, s7)
+	assert.Equal(t, e7, r7)
+
+	s8 := point{x: -7, y: -2}
+	p8 := path{direction: "U", length: 3}
+	e8 := []point{{x: -7, y: -1}, {x: -7, y: 0}, {x: -7, y: 1}}
+	r8 := pathToPoints(p8, s8)
+	assert.Equal(t, e8, r8)
+}
