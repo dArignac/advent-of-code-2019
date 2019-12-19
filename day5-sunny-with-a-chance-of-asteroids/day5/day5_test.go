852a03ce1580a58b21e2656bdcede8bc19592dad
Alexander Herrmann <darignac@gmail.com>
2019-12-19 15:31:16 +0100

    refactored test structure

--- day5-sunny-with-a-chance-of-asteroids/day5/day5_test.go
+++ day5-sunny-with-a-chance-of-asteroids/day5/day5_test.go
@@ -6,16 +6,18 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestFixIntcodeProgramCode(t *testing.T) {
+func TestFixIntcodeProgramCodeNoop(t *testing.T) {
 	code := []int{99}
 	output := []int{7}
 	assert.Equal(t, []int{99}, RunProgramCode(code, 0, 0, &output))
 	assert.Equal(t, []int{7}, output)
+}
 
-	output = []int{}
+func TestFixIntcodeProgramCodeAddition(t *testing.T) {
+	output := []int{}
 
 	// all params as positions
-	code = []int{1, 0, 0, 0, 99}
+	code := []int{1, 0, 0, 0, 99}
 	assert.Equal(t, []int{2, 0, 0, 0, 99}, RunProgramCode(code, 0, 0, &output))
 
 	// all params as values
@@ -30,15 +32,25 @@ func TestFixIntcodeProgramCode(t *testing.T) {
 	code = []int{101, 3, 0, 0, 99}
 	assert.Equal(t, []int{3 + 101, 3, 0, 0, 99}, RunProgramCode(code, 0, 0, &output))
 
-	code = []int{2, 3, 0, 3, 99}
+	// day 3
+	code = []int{1, 1, 1, 4, 99, 5, 6, 0, 99}
+	assert.Equal(t, []int{30, 1, 1, 4, 2, 5, 6, 0, 99}, RunProgramCode(code, 0, 0, &output))
+
+	// day 5
+	code = []int{1101, 100, -1, 4, 0}
+	assert.Equal(t, []int{1101, 100, -1, 4, 99}, RunProgramCode(code, 0, 0, &output))
+}
+
+func TestFixIntcodeProgramCodeMultiplication(t *testing.T) {
+	output := []int{}
+	code := []int{2, 3, 0, 3, 99}
 	assert.Equal(t, []int{2, 3, 0, 6, 99}, RunProgramCode(code, 0, 0, &output))
 
 	code = []int{2, 4, 4, 5, 99, 0}
 	assert.Equal(t, []int{2, 4, 4, 5, 99, 9801}, RunProgramCode(code, 0, 0, &output))
+}
 
-	code = []int{1, 1, 1, 4, 99, 5, 6, 0, 99}
-	assert.Equal(t, []int{30, 1, 1, 4, 2, 5, 6, 0, 99}, RunProgramCode(code, 0, 0, &output))
-
+func TestFixIntcodeProgramCodeInputOutput(t *testing.T) {
 	// code = []int{3, 0, 4, 0, 99}
 	// RunProgramCode(code, 666, 0, &output)
 	// assert.Equal(t, output, []int{666})
@@ -46,7 +58,4 @@ func TestFixIntcodeProgramCode(t *testing.T) {
 	// code = []int{1002, 4, 3, 4, 33}
 	// output = []int{}
 	// assert.Equal(t, []int{1002, 4, 3, 4, 99}, RunProgramCode(code, 0, 0, &output))
-
-	// code = []int{1101, 100, -1, 4, 0}
-	// assert.Equal(t, []int{1101, 100, -1, 4, 99}, RunProgramCode(code, 0, 0, &output))
 }
